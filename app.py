b571a0ed8632b723e14ef0fbb261b2a465c5a5d0
lisset21hernandez <101090747+lisset21hernandez@users.noreply.github.com>
2022-03-07 08:21:13 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+
+from flask import Flask, jsonify, request
+
+app = Flask()
+
+@app.route('/imc', methods=['GET'])
+def calcularIMC():
+    peso = float(request.args.get('peso'))
+    altura = float(request.args.get('altura'))
+    resultado = peso/(altura*altura)
+    return jsonify({"value",resultado})
+    
+@app.route('/igc', methods=['GET'])
+def calcularIGC():
+    cint = float(request.args.get('cint')) 
+    altura = float(request.args.get('alt'))
+    resultado = cint/(altura*(altura**(0.5)))
+    return jsonify({"value",resultado})
+
+
+@app.route('/calorias', methods=['GET'])
+def calcularCalorias():
+    peso = float(request.args.get('peso'))
+    distancia = float(request.args.get('distancia'))
+    resultado = 1.03*peso*distancia
+    return jsonify({"value",resultado})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
