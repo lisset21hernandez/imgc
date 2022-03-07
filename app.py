be2b9822a9a81d6a84b911f8fb52253f4720f2a4
lisset21hernandez <101090747+lisset21hernandez@users.noreply.github.com>
2022-03-07 08:52:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ def calcularIMC():
     peso = float(request.args.get('peso'))
     altura = float(request.args.get('altura'))
     resultado = peso/(altura*altura)
-    return jsonify({"value",resultado})
+    return jsonify({"value":resultado})
     
 @app.route('/igc/', methods=['GET'])
 def calcularIGC():
     cint = float(request.args.get('cint')) 
     altura = float(request.args.get('alt'))
     resultado = cint/(altura*(altura**(0.5)))
-    return jsonify({"value",resultado})
+    return jsonify({"value":resultado})
 
 
 @app.route('/calorias/', methods=['GET'])
@@ -23,7 +23,7 @@ def calcularCalorias():
     peso = float(request.args.get('peso'))
     distancia = float(request.args.get('distancia'))
     resultado = 1.03*peso*distancia
-    return jsonify({"value",resultado})
+    return jsonify({"value":resultado})
 
 
 if __name__ == '__main__':
